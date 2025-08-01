e6edff09c0ae3a7f77239c95abc77bae44728420
jaejun <jaejun.pak@gmail.com>
2025-08-01 21:32:19 +0900

    feat: auth 라우터에 Joi 유효성 검사 미들웨어 적용

--- src/routes/auth.router.js
+++ src/routes/auth.router.js
@@ -1,12 +1,13 @@
 import express from 'express';
 import { signup, login } from '../controllers/authController.js';
+import { signupSchema, loginSchema, validateRequest } from '../validations/auth.validation.js';
 
 const router = express.Router();
 
 // 회원가입
-router.post('/signup', signup);
+router.post('/signup', validateRequest(signupSchema), signup);
 
 // 로그인
-router.post('/login', login);
+router.post('/login', validateRequest(loginSchema), login);
 
 export default router; 
